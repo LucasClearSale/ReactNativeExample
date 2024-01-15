887987c62454dc702aca1b0a10ee7368b6a1818c
Lucas de Castro Carvalho <lucas.decarvalho@MACDEV008942.local>
2024-01-15 13:04:07 -0300

    Project now can called Android SDK

--- android/app/src/main/java/com/iosreact/MainActivity.kt
+++ android/app/src/main/java/com/iosreact/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.iosreact
 
+import android.os.Bundle
+import android.util.Log
+import com.clear.studio.csdocs.entries.CSDocumentoscopy
+import com.clear.studio.csdocs.entries.CSDocumentoscopySDK
+import com.clear.studio.csdocs.entries.CSDocumentoscopySDKError
+import com.clear.studio.csdocs.entries.CSDocumentoscopySDKListener
+import com.clear.studio.csdocs.entries.CSDocumentoscopySDKResult
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -19,4 +26,25 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        CSDocumentoscopySDK.initialize(this, CSDocumentoscopy(clientID = "TAG", clientSecret = "TAG", cpf = "TAG", identifierId = "TAG"), object:
+            CSDocumentoscopySDKListener {
+            override fun didOpen(){
+                Log.d("TAG", "SDK Foi Aberto")
+            }
+            override fun didTapClose(){
+                Log.d("TAG", "Usuário encerrou manualmente o fluxo")
+            }
+            override fun didReceiveError(error : CSDocumentoscopySDKError){
+                val errorText = "${error.text} - errorCode: ${error.errorCode}"
+                Log.d("TAG", "Error: $errorText")
+            }
+            override fun didFinishCapture(result: CSDocumentoscopySDKResult){
+                val sessionIdText = "Session Id: " + result.sessionId
+            }
+        })
+    }
 }
