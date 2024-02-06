4dbbef21e33fcecc05b00799fb22ce5da6df375b
Lucas de Castro Carvalho <lucas.decarvalho@MACDEV008942.local>
2024-02-06 00:00:29 -0300

    called SDK

--- ios/iOSReact/AppDelegate.h
+++ ios/iOSReact/AppDelegate.h
@@ -1,9 +1,6 @@
 #import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
-#import "RCTBridgeModule.h"
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate,RCTBridgeModule>
-
-@property (nonatomic, strong) UIWindow *window;
+@interface AppDelegate : RCTAppDelegate
 
 @end
