25c1ca12e531de7af1835a7a308773a4f0197c70
Lucas de Castro Carvalho <lucas.decarvalho@MACDEV008942.local>
2024-02-06 09:03:23 -0300

    call sdk after 1.5 seconds

--- ios/iOSReact/AppDelegate.mm
+++ ios/iOSReact/AppDelegate.mm
@@ -10,8 +10,11 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-  CSDocumentoscopyBridget *sdk = [[CSDocumentoscopyBridget alloc] init];
-  [sdk callSDKWithViewController: self.createRootViewController];
+  
+  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    CSDocumentoscopyBridget *sdk = [[CSDocumentoscopyBridget alloc] init];
+    [sdk callSDKWithViewController: self.window.rootViewController];
+  });
   
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
